c3fb26b8fbb7836854150b3c49aa0e102c42f84b
Jens Ihrig <jens.ihrig@mail.de>
2022-12-27 10:20:29 +0100

    updated cv

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,53 +1,100 @@
 module.exports = {
-  name: 'Jens Ihrig',
-  title: 'Senior Frontend Engineer',
+  name: "Jens Ihrig",
+  title: "Senior Frontend Engineer",
   facts: {
-    'Ak. Grad': { 
-        icon: 'graduation-cap',
-        text: 'Diplom Informatiker (FH)' 
+    "Ak. Grad": {
+      icon: "graduation-cap",
+      text: "Diplom Informatiker (FH)",
     },
-    'Geburtstag': { 
-        icon: 'asterisk',
-        text:  '09.01.1977'  
+    Geburtstag: {
+      icon: "asterisk",
+      text: "09.01.1977",
     },
-    'Wohnort': { 
-        icon: 'home',
-        text: 'Freiburg im Breisgau' 
+    Wohnort: {
+      icon: "home",
+      text: "Freiburg im Breisgau",
     },
-    'E-Mail': { 
-        icon: 'envelope',
-        text: '<a href="mailto:lebenslauf@jensihrig.de">lebenslauf@jensihrig.de</a>' 
+    "E-Mail": {
+      icon: "envelope",
+      text: '<a href="mailto:lebenslauf@jensihrig.de">lebenslauf@jensihrig.de</a>',
     },
   },
   techskills: [
-    ['HTML5', '+++++'],
-    ['CSS3', '++++'],
-    ['JSS', '++'],
-    ['Javascript', '+++++'],
-    ['Typescript', '++++'],
-    ['React.js', '+++++'],
-    ['Redux', '++++'],
-    ['Jest', '++'],
-    ['Cypress', '+'],
-    ['Webpack', '+++'],
-    ['CI/DevOps', '+'],
+    ["HTML5", "+++++"],
+    ["CSS3", "++++"],
+    ["JSS", "++"],
+    ["Javascript", "+++++"],
+    ["Typescript", "++++"],
+    ["React.js", "+++++"],
+    ["Redux", "++++"],
+    ["Jest", "++"],
+    ["Cypress", "++"],
+    ["Webpack", "+++"],
+    ["CI/DevOps", "+"],
+    ["Magnolia", "+"],
+    ["HUGO", "++"],
   ],
 
   methodskills: [
-    ['Scrum', '++++'],
-    ['Kanban', '++'],
-    ['Clean Code', '++++'],
-    ['TDD', '+'],
-    ['XP', '+'],
-    ['DDD', '+'],
+    ["Scrum", "++++"],
+    ["Kanban", "++"],
+    ["Clean Code", "++++"],
+    ["TDD", "+"],
+    ["XP", "+"],
+    ["DDD", "+"],
   ],
 
   jobs: [
     {
-      title: 'Senior Frontend Developer & Teamlead',
-      company: 'ETECTURE GmbH, Karlsruhe',
-      datespan: 'Januar 2016 bis heute',
-      badges: ['react', 'javascript', 'typescript' , 'html', 'css', 'scrum' , 'kanban' , 'cleancode', 'xp', 'tdd' , 'ddd'],
+      title: "Senior Frontend Engineer",
+      company: "JobRad GmbH, Freiburg im Breisgau",
+      datespan: "September 2022 bis heute",
+      badges: ["javascript", "html", "css", "kanban", "cleancode", "magnolia"],
+      contents: `
+* Technische Verantwortung und Betrieb der Unternehmenswebsite sowie des zugrunde liegenden Content Management Systems Magnolia als Mitglied des neu gegründeten CMS Platform Teams im Bereich Digitalisierung und Systeme
+* Weiterentwicklung des Magnolia Frontends mittels HTML5, Javascript, CSS3 und Freemarker 
+* Mitglied der A11Y Taskforce, welche die Verbesserung der Barrierefreiheit von Portalen und der Unternehmenswebsite sowie die Festlegung von A11Y Standards für das Unternehmen zum Ziel hatte
+* Beratung bei der Auswahl von Bewerbern
+* Durchführung von Bewerbungsgesprächen und Coding Interviews
+      `,
+    },
+    {
+      title: "Senior UX Engineer",
+      company: "JobRad GmbH, Freiburg im Breisgau",
+      datespan: "November 2021 bis August 2022",
+      badges: [
+        "javascript",
+        "html",
+        "css",
+        "kanban",
+        "cleancode",
+        "magnolia",
+        "react",
+      ],
+      contents: `
+* Technische Verantwortung und Betrieb der Unternehmenswebsite sowie des zugrunde liegenden Content Management Systems Magnolia als Mitglied im Team Online Marketing
+* Weiterentwicklung des Magnolia Frontends  mittels HTML5, Javascript, CSS3 und Freemarker 
+* Proof of Concept für die Headless Ausspielung von Content aus Magnolia heraus in React.js basierte Portale
+* Technische Beratung bei der Entwicklung eines Design Systems für das Unternehmen
+      `,
+    },
+    {
+      title: "Senior Frontend Developer & Teamlead",
+      company: "ETECTURE GmbH, Karlsruhe",
+      datespan: "Januar 2016 bis Oktober 2021",
+      badges: [
+        "react",
+        "javascript",
+        "typescript",
+        "html",
+        "css",
+        "scrum",
+        "kanban",
+        "cleancode",
+        "xp",
+        "tdd",
+        "ddd",
+      ],
       contents: `
 #### Softwareentwicklung 
 
@@ -73,84 +120,80 @@ module.exports = {
 * Mitbestimmung bei der technologischen und methodischen Ausrichtung des Standorts
 * Standortweite Prozessentwicklung (z.B. Staffing, Checkliste Best Practices für Projektdurchführung etc.)
 * Unterstützende technologische Einschätzung von New Business sowie Mitwirkung bei der Erstellung von Angeboten
-`
+`,
     },
     {
-      title: 'Web-Developer',
-      company: 'Key-Work Consulting GmbH, Karlsruhe',
-      datespan: 'Juli 2003 bis Dezember 2015',
-      badges: ['javascript', 'html', 'css', 'kanban', 'cleancode'],
+      title: "Web-Developer",
+      company: "Key-Work Consulting GmbH, Karlsruhe",
+      datespan: "Juli 2003 bis Dezember 2015",
+      badges: ["javascript", "html", "css", "kanban", "cleancode"],
       contents: `
 * Konzeption und Entwicklung von Web-Anwendungen auf Basis von PHP, Java EE, OfBiz und Grails
 * Schwerpunkt dabei im Frontend-Bereich mittels HTML5, Javascript, JQuery, CSS3, Highcharts, JSP, Freemarker und GSP  
 * Vereinheitlichung, Standardisierung und Modernisierung der Web-Oberflächen von Anwendungen aus verschiedenen Systemen innerhalb von Portalen
 
-      `
+      `,
     },
     {
-      title: 'Praktikant IT Webdevelopment',
-      company: 'IBM e-business Innovation Center, Hamburg',
-      datespan: 'Juni 2001 - Oktober 2001',
-      badges: ['html', 'javascript', 'css'],
+      title: "Praktikant IT Webdevelopment",
+      company: "IBM e-business Innovation Center, Hamburg",
+      datespan: "Juni 2001 - Oktober 2001",
+      badges: ["html", "javascript", "css"],
       contents: `
-Entwicklung von Anwendungsoberflächen mittels HTML, CSS, Javascript und Flash
-`
+Entwicklung von Anwendungsoberflächen und Websites mittels HTML, CSS, Javascript und Macromedia Flash
+`,
     },
     {
-      title: 'Werkstudent',
-      company: 'net-solution GmbH, Heddesheim',
-      datespan: 'Januar 2000 - Dezember 2001',
-      badges: ['html', 'javascript', 'css'],
+      title: "Werkstudent",
+      company: "net-solution GmbH, Heddesheim",
+      datespan: "Januar 2000 - Dezember 2001",
+      badges: ["html", "javascript", "css"],
       contents: `
-HTML- und Javascript-Entwicklung
-`
-    }
+Entwicklung diverser Unternehmenswebsites mittels HTML, CSS, Javascript
+`,
+    },
   ],
 
-
-  education: [{
-    title: 'Medieninformatik',
-    org: 'Fachhochschule Heidelberg',
-    datespan: 'Oktober 1999 - September 2002',
-    contents: `
+  education: [
+    {
+      title: "Medieninformatik",
+      org: "Fachhochschule Heidelberg",
+      datespan: "Oktober 1999 - September 2002",
+      contents: `
 Diplomarbeit "Neuentwicklung und Bewertung von Interaktionselementen" in der Usability-Abteilung der IBM Entwicklung GmbH, Böblingen
-`
-  },
-{
-  title: 'Kommunikationsinformatik',
-  org: 'Fachhochschule Harz, Wernigerode',
-  datespan: 'Oktober 1998 - Mai 1999',
-  contents: ``
-},
-{
-  title: 'Abitur',
-  org: 'Hebel Gymnasium, Schwetzingen',
-  datespan: '1987 - 1997',
-  contents: ``
-}
-],
-
-
+`,
+    },
+    {
+      title: "Kommunikationsinformatik",
+      org: "Fachhochschule Harz, Wernigerode",
+      datespan: "Oktober 1998 - Mai 1999",
+      contents: ``,
+    },
+    {
+      title: "Abitur",
+      org: "Hebel Gymnasium, Schwetzingen",
+      datespan: "1987 - 1997",
+      contents: ``,
+    },
+  ],
 
   experience: [
     {
-      header: 'Sprachkenntnisse',
+      header: "Sprachkenntnisse",
       contents: `
 * Deutsch (C2)
 * Englisch (B2-C1) 
-`
+`,
     },
-
 
     {
-      header: 'Hobbies',
+      header: "Hobbies",
       contents: `
+*	Kochen und Foodblogging
+*	Reisen
 *	(elektronische) Musik
 *	Joggen
-*	Reisen
-*	Kochen und Foodblogging
-*	Fotografie
-      `
+      `,
     },
-  ]
+  ],
 };
